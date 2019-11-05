e00e5c4c6f572cb2c2b03d9db9650b4c2c55f68d
Usong Cho <silvermouse4012@gmail.com>
2019-11-04 20:09:50 -0800

    Layout, Aux, BurgerBuilder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <p>testing one two three</p>
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
             </div>
         )
     }
 } 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
