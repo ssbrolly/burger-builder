cd7336633d6378cb0521e4ebb0cced5c8859e230
Usong Cho <silvermouse4012@gmail.com>
2019-11-16 18:56:12 -0800

    iproved performance by only updating Modal when it is visible

--- src/components/Burger/OrderedSummary/OrderedSummary.js
+++ src/components/Burger/OrderedSummary/OrderedSummary.js
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderedSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKeys => {
-        return (
-            <li key={igKeys}>
-                <span style={{ textTransform: 'capitalize' }}>{igKeys}</span>
-                : {props.ingredients[igKeys]}
-            </li>
-        );
-    });
+class OrderedSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderedSummary] will update')
+    }
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Your Total is: ${props.total.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled} >CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKeys => {
+            return (
+                <li key={igKeys}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKeys}</span>
+                    : {this.props.ingredients[igKeys]}
+                </li>
+            );
+        });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Your Total is: ${this.props.total.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled} >CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux >
+        );
+    }
+
+
 };
 
-export default orderedSummary;
+export default OrderedSummary;
 
 
 
