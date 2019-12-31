d55e07e74817ff8f880eddad26d24e7b638ac4f3
Usong Cho <silvermouse4012@gmail.com>
2019-12-30 16:00:46 -0800

    withErrorHandler

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
                 </Layout>
             </div>
         )
-    }
-}
+    };
+};
 
 export default App;
 
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,14 +9,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
         };
 
         componentDidMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptors = axios.interceptors.request.use(req => {
                 this.setState({ error: null })
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptors = axios.interceptors.response.use(res => res, error => {
                 this.setState({ error: error })
                 return error;
             });
+        };
+
+        componentWillUnmount() {
+            console.log('will unmount', this.reqInterceptors, this.resInterceptors);
+            axios.interceptors.request.eject(this.reqInterceptors);
+            axios.interceptors.request.eject(this.resInterceptors);
         };
 
         errorConfirmedHandler = () => {
