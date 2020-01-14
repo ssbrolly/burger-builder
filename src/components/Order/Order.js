f82e3426b7e4925fb575791cfe02a79a3e210660
Usong Cho <silvermouse4012@gmail.com>
2020-01-13 17:22:49 -0800

    added Order and Orders

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Order.module.css';
+
+
+const order = (props) => (
+    <div className={classes.Order}>
+        <p>Ingredient: Salad (1)</p>
+        <p>Price: <strong>USD 5.54</strong></p>
+    </div>
+);
+
+export default order;
