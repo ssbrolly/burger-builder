d36519e737dd65ba6837e57aadc6a18215be4b2c
Usong Cho <silvermouse4012@gmail.com>
2020-01-26 15:31:55 -0800

    added Orders and order components

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import axios from '../../axios-orders';
-import Order from '../../components/Order/Order'
+import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
-    componentDidMount() {
+    state = {
+        orders: [],
+        loading: true,
+    };
 
-    }
+    componentDidMount() {
+        axios.get('orders.json')
+            .then(res => {
+                const fetchedOrders = []
+                fetchedOrders.push(Object.keys(res.data))
+                // for (let key in res.data) {
+                //     fetchedOrders.push({
+                //         ...res.data[key],
+                //         id: key
+                //     })
+                // }
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch(err => {
+                this.setState({ loading: false })
+            });
+    };
 
     render() {
+        console.log(this.state.orders)
         return (
             <div>
-                <Order />
-                <Order />
+                <Order orders={this.state.orders[0]} />
+                <Order orders={this.state.orders[1]} />
             </div>
         );
     };
 };
-export default Orders;
+export default withErrorHandler(Orders, axios);
